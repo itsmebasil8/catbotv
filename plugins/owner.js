15b587c4cab39c0444ccb4b819d403f0c07561e9
itsmebasil8 <92202940+itsmebasil8@users.noreply.github.com>
2021-12-07 20:34:20 +0530

    ᴄᴀᴛʙᴏᴛ

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,10 +1,3 @@
-/* Copyright (C) 2021 AFNAN PALLIKKEL
-CODDED AFNAN PALLIKKEL
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-PINKY
-*/
-
 const Asena = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
 const {spawnSync} = require('child_process');
@@ -46,7 +39,7 @@ else if (Config.WORKTYPE == 'public') {
             await message.client.sendMessage(message.jid,'CATBOT CREATED BY *BASILSER*' , MessageType.text);
         }
         else {
-            await message.client.sendMessage(message.jid,Config.PLK + '\n\n--------------------', MessageType.text);
+            await message.client.sendMessage(message.jid,Config.PLK + '*HE IS MY BOSS*', MessageType.text);
         }
     }));
 }
